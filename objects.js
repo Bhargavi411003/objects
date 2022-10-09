76b66fec8e50330e8500ec4acd883fcb91a2e9ba
Bhargavi411003 <8497924886bharu@gmail.com>
2022-10-09 20:29:08 +0530

    first commit

---
+++ objects.js
@@ -0,0 +1,20 @@
+const person1 = {
+    programming:['javascript','python','ruby','go'],
+    languages:['ischallenging','difficulty','isfun','rewarding'],
+    count:[true],
+    number:[7]
+
+}
+console.log(person1.programming);
+console.log(person1.programming[0])
+console.log(person1.programming[1])
+console.log(person1.programming[2])
+console.log(person1.programming[3])
+console.log(person1.count[0])
+console.log(person1.number[0])
+console.log(person1.count[0])
+console.log(person1.languages[0])
+console.log(person1.languages[1])
+console.log(person1.languages[2])
+console.log(person1.languages[3])
+console.log(person1[1])
